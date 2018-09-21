7f8cf4675825fb12dbe6770a6a8346cc9649e1c9
growdeuno <grow.deuno@gmail.com>
2018-09-21 13:46:13 +0800

    Return effect

--- return.js
+++ return.js
@@ -4,3 +4,30 @@ function name(){
 var intro = name();
 
 console.log(intro);
+
+console.log("\n");
+
+
+function boo(){
+  return "Majin Boo";
+  console.log("Tes"); //isn't proceed
+}
+console.log(boo());
+console.log("\n");
+
+
+function coo(){
+  console.log("Tes"); //isn't proceed
+  return "Majin Coo";
+
+}
+
+console.log(coo());
+console.log("\n");
+
+
+function foo(){
+  var laugh = "Fufufufufu";
+  return laugh;
+}
+console.log(foo());
